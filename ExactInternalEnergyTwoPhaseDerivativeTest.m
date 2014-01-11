867dc248159c1ac2d2a48e5efaa8a16163233030
Troy Haskin <git.troy@hask.in>
2014-01-10 18:31:59 -0600

    Edit: Finally get analytical deriv. correct

--- ExactInternalEnergyTwoPhaseDerivativeTest.m
+++ ExactInternalEnergyTwoPhaseDerivativeTest.m
@@ -1,54 +1,72 @@
-clc;
+% clc;
 clear('all');
 
 T = 300;
 tau = CriticalTemperature./T;
 [Psat,rhol,rhog] = SaturationStateGivenTsat(T);
 
-rho = 0.9999*rhol;
+rho = 100 * rhog;
 del  = rho  / CriticalDensity();
 delL = rhol / CriticalDensity();
 delG = rhog / CriticalDensity();
 
-dT = 1E-5;
+dT = 1E-9;
 i_TN = (InternalEnergy(rho,T+dT)-InternalEnergy(rho,T-dT))./(2*dT);
 tau_T = tau_Temperature(T);
 i_tauN = i_TN./tau_T;
 
-il      = InternalEnergyOne(rhol,T);
-ig      = InternalEnergyOne(rhog,T);
-il_del  = InternalEnergyOne_delta(rhol,T);
-ig_del  = InternalEnergyOne_delta(rhog,T);
-il_tau  = InternalEnergyOne_tau(rhol,T);
-ig_tau  = InternalEnergyOne_tau(rhog,T);
+iL      = InternalEnergyOne(rhol,T);
+iG      = InternalEnergyOne(rhog,T);
 
-x       = QualityFromDensity(rho,rhol,rhog);
-Pnd     = Psat/DimensioningPressure();
-Pnd_tau = ClausiusClapeyronRRND(Psat,tau,delL,delG,il,ig);
-PhiR_d  = HelmholtzResidual_d ([delL;delG],tau);
-PhiR_dd = HelmholtzResidual_dd([delL;delG],tau);
-PhiR_dt = HelmholtzResidual_dt([delL;delG],tau);
-dl_tau  = (Pnd + tau.*Pnd_tau-delL.^2.*PhiR_dt(1))./(1+2.*delL.*PhiR_d(1)+delL.^2.*PhiR_dd(1));
-dg_tau  = (Pnd + tau.*Pnd_tau-delG.^2.*PhiR_dt(2))./(1+2.*delG.*PhiR_d(2)+delG.^2.*PhiR_dd(2));
+x        = QualityFromDensity(rho,rhol,rhog);
+Pnd      = Psat/DimensioningPressure();
+Pnd_tau  = ClausiusClapeyronRRND(Psat,tau,delL,delG,iL,iG);
+PhiR_d   = HelmholtzResidual_d ([delL;delG],tau);
+PhiR_dd  = HelmholtzResidual_dd([delL;delG],tau);
+PhiR_dt  = HelmholtzResidual_dt([delL;delG],tau);
+delL_tau = (delL + tau.^2 .* Pnd_tau + del.^2 .* (PhiR_d(1) - tau .* PhiR_dt(1))) ./ ...
+           (tau .* (1 + 2 * del .* PhiR_d(1) + del.^2 .* PhiR_dd(1)));
+delG_tau = (delG + tau.^2 .* Pnd_tau + del.^2 .* (PhiR_d(2) - tau .* PhiR_dt(2))) ./ ...
+           (tau .* (1 + 2 * del .* PhiR_d(2) + del.^2 .* PhiR_dd(2)));
+       
+iL_del  = InternalEnergyOne_delta(rhol,T);
+iG_del  = InternalEnergyOne_delta(rhog,T);
+iL_tau  = iL_del .* delL_tau + InternalEnergyOne_tau(rhol,T);
+iG_tau  = iG_del .* delG_tau + InternalEnergyOne_tau(rhog,T);
 
-x_tau = ((delL-del).*delL.*dg_tau + (del-delG).*delG.*dl_tau)./(del.*(delG-delL).^2);
+x_tau = ((delL-del).*delL.*delG_tau + (del-delG).*delG.*delL_tau)./(del.*(delG-delL).^2);
 
-i_tauExact = x_tau.*(il - ig) + (1-x).*(ig_tau + dg_tau.*ig_del) + x.*(il_tau + dl_tau.*il_del);
-i_TExact   = -i_tauExact.*tau_T;
+i_tauExact = iL_tau + x .* (iG_tau - iL_tau) + x_tau .* (iG - iL);
+i_TExact   = i_tauExact.*tau_T;
 
-Show(i_tauN - i_tauExact);
-Show((i_tauN - i_tauExact)./i_tauExact);
+drho = 1E-9;
+i_rhoN  = (InternalEnergy(rho + drho,T)-InternalEnergy(rho - drho,T))./(2*drho);
+rho_del =  CriticalDensity();
+i_delN  = i_rhoN .* rho_del;
+
+x_del = delG .* delL / (del.^2 .* (delG - delL));
+i_del = x_del .* (iG - iL);
+i_rho = i_del / CriticalDensity();
+
+
+Show([(i_tauN - i_tauExact);(i_tauN - i_tauExact)./i_tauExact]);
+Show([(i_delN - i_del);(i_delN - i_del)/i_del]);
 
 
 subplot(2,1,1);
-Tplot = linspace(-10,10)+T;
+Tcen  = T;
+iCen  = InternalEnergy(rho,Tcen);
+Tplot = (0.999999:0.00000001:1.000001)' * Tcen;
 iPlot = InternalEnergy(rho,Tplot);
-iTangent = i_TN*(Tplot-T)+InternalEnergy(rho,T);
-plot(Tplot,iPlot,Tplot,iTangent,'ro');
+plot(Tplot,iPlot,Tcen,iCen,'o',Tplot,i_TExact*(Tplot-Tcen) + iCen);
+
 
 subplot(2,1,2);
-Tplot = linspace(-10,10)+T;
-iPlot = InternalEnergy(rho,Tplot);
-iTangent = i_TExact*(Tplot-T)+InternalEnergy(rho,T);
-plot(Tplot,iPlot,Tplot,iTangent,'ro');
+rhoCen = rho;
+iCen  = InternalEnergy(rhoCen,T);
+rhoPlot = (0.99:0.01:1.01)' * rho;
+iPlot = InternalEnergy(rhoPlot,T);
+plot(rhoPlot,iPlot,rhoCen,iCen,'o',rhoPlot,i_rho*(rhoPlot-rhoCen) + iCen);
+
+
 
