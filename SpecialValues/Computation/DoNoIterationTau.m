f29f510e1f93ca244a4361121452747e8833bdf0
Troy Haskin <git.troy@hask.in>
2015-04-01 22:17:09 -0500

    tau iteration limit reimplemented

--- SpecialValues/Computation/DoNoIterationTau.m
+++ SpecialValues/Computation/DoNoIterationTau.m
@@ -2,5 +2,5 @@ function tau = DoNoIterationTau()
     %   Any temperature above this value will use the curve fits and not 
     %   iteration for the saturation properties due to an inability to 
     %   get the system to robustly converge above the value;
-    tau = 0.9;%1.0110875;
+    tau = 1.0110875;
 end
