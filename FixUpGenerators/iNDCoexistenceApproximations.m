c17c7c0a8e86212dacafeac0c1ad6750c8f5d708
Troy Haskin <git.troy@hask.in>
2015-04-20 20:52:31 -0500

    Saturation approximation done; melting line next.

--- FixUpGenerators/iNDCoexistenceApproximations.m
+++ FixUpGenerators/iNDCoexistenceApproximations.m
@@ -2,66 +2,167 @@ clc;
 
 % clear();
 % Tc  = CriticalTemperature();
-% tau = Tc./[linspace(273.16,640,1E3),linspace(640.001,Tc,2E3)]';
+% tau = Tc./[linspace(273.16,280,200),linspace(280.01,640,1E3),linspace(640.001,Tc,2E3)]';
 % [P,delL,delG] = SaturationStateGivenTauRRND(tau);
 % iL = InternalEnergyOneRND(delL,tau);
 % iG = InternalEnergyOneRND(delG,tau);
 
 
-% %   Region 1
-% mask     = [1,10,300,301,644,645];
-% logDelG  = log(delG(mask));
-% iGfit    = iG(mask);
-% iGfit([2,3,4]) = 1.00008*iGfit([2,3,4]);
-% [p,S,mu] = polyfit(logDelG,iGfit,5);
-% mask  = mask(1):mask(end);
-% delG1 = delG(mask);
-% iG1   = polyval(p,log(delG1),S,mu);
-% % semilogx(delG(mask),iG(mask),delG(mask),iGfit);
-% % legend('Exact','Fit');
-% % all(iGfit >= iG(mask))
-% 
-% %   Region 2
-% mask     = [645,646,900,1000,1800,2990,numel(delG)];
-% delGfit  = delG(mask);
-% iGfit    = iG(mask);
-% iGfit([3,4,5]) = 1.0020*iGfit([3,4,5]);
-% iGfit(end-1) = 1.0005*iGfit(end-1);
-% iGfit(end) = 1.000000000000001*iGfit(end);
-% [p,S,mu] = polyfit(delGfit,iGfit,6);
-% mask  = mask(1):mask(end);
-% delG2 = delG(mask);
-% iG2   = polyval(p,delG2,S,mu);
-% % plot(delG(mask(1):mask(end)),iG(mask(1):mask(end)),delG2,iG2);
-% % legend('Exact','Fit');
-% % Show((iG2 - iG(mask))');
-% % all(iG2 >= iG(mask))
-% 
-% semilogx(delG,iG,[delG1;delG2],[iG1;iG2]);
-% all([iG1;iG2(2:end)] >= iG)
+% =================================================== %
+%                      Gas Region                     %
+% =================================================== %
+
+% --------------------------- %
+%           Region 1          %
+% --------------------------- %
+%   Indices of interest
+mask     = [1,210,450,501,837,838];
+logDelG  = log(delG(mask));
+iGfit    = iG(mask);
+%   Correction to ensure over-prediction
+iGfit([2,3,4]) = 1.0006*iGfit([2,3,4]);
+iGfit(6) = 1.000000000000001*iGfit(6);
+%   Determine coefficients
+[p,S,mu] = polyfit(logDelG,iGfit,5);
+%   Contract
+mask  = mask(1):mask(end);
+delG1 = delG(mask);
+iG1   = polyval(p,log(delG1),S,mu);
 
 
-% %   Region 1
-% mask  = [350,675,2000,3000];
-% iLfit = iL(mask);
-% [p,S,mu] = polyfit(delL(mask),iLfit,3);
-% mask = mask(1):mask(end);
-% delL1 = delL(mask);
-% iL1   = polyval(p,delL1,S,mu);
-% shift = 0.02480;
-% plot(delL(mask),iL(mask),delL1,iL1+shift);
-% Show([mask;(iL1 - iL(mask))']);
-% all((iL1+shift) >= iL(mask))
+% --------------------------- %
+%           Region 2          %
+% --------------------------- %
+%   Indices of interest
+mask     = [838,839,1093,1193,1993,3183,numel(delG)];
+delGfit  = delG(mask);
+iGfit    = iG(mask);
+%   Correction to ensure over-prediction
+iGfit([3,4,5]) = 1.0020*iGfit([3,4,5]);
+iGfit(end-1) = 1.0006*iGfit(end-1);
+iGfit(end) = 1.000000000000001*iGfit(end);
+%   Determine coefficients
+[p,S,mu] = polyfit(delGfit,iGfit,6);
+%   Contract
+mask  = mask(1):mask(end);
+delG2 = delG(mask);
+iG2   = polyval(p,delG2,S,mu);
 
 
-%   Region 2
-mask  = [12,50,100,200,350];
+
+
+
+
+% =================================================== %
+%                    Liquid Region                    %
+% =================================================== %
+
+% --------------------------- %
+%           Region 1          %
+% --------------------------- %
+%   Indices of interest
+mask  = [206,207,1000,1800,2800,numel(delL)];
 iLfit = iL(mask);
-[p,S,mu] = polyfit(delL(mask).^2,iLfit,3);
+%   Correction to ensure over-prediction
+iLfit(1) = 1.000000001*iLfit(1);
+iLfit(3) = 1.002*iLfit(3);
+iLfit(4) = 1.002*iLfit(4);
+iLfit(5) = 1.002*iLfit(5);
+iLfit(end) = 1.000000000000001*iLfit(end);
+%   Determine coefficients
+[p,S,mu] = polyfit(delL(mask),iLfit.^2,5);
+%   Contract
 mask = mask(1):mask(end);
 delL1 = delL(mask);
-iL1   = polyval(p,delL1.^2,S,mu);
-shift = 0*0.02;
-plot(delL(mask),iL(mask),delL1,iL1+shift);
-Show([mask;(iL1 - iL(mask))']);
-all((iL1+shift) >= iL(mask))
+iL1   = sqrt(polyval(p,delL1,S,mu));
+
+
+% --------------------------- %
+%           Region 2          %
+% --------------------------- %
+%   Determine fit coefficients
+mask  = [117,200,206];
+mask = mask(1):mask(end);
+correlation = @(c,x) c(2)*(max(delL(mask)) - x).^c(1).*exp(c(3)*(max(delL) - x))+iL(117);
+objective   = @(c) norm(correlation(c,delL(mask)) - iL(mask),2);
+c = [0.49,2.67,15];
+c = fminsearch(objective,c);
+%   Corrections to ensure over-prediction
+c(1) = 0.980*c(1);
+c(2) = 0.927*c(2);
+%   Contract
+delL2 = delL(mask);
+iL2  = correlation(c,delL2);
+
+
+
+% --------------------------- %
+%           Region 3          %
+% --------------------------- %
+%   Determine fit coefficients
+mask  = [1,117];
+mask = mask(1):mask(end);
+correlation = @(c,x) c(2)*(max(delL(mask)) - x).^c(1).*exp(c(3)*(max(delL) - x))+iL(117);
+objective   = @(c) norm(correlation(c,delL(mask)) - iL(mask),2);
+c = [0.49,2.67,15];
+c = fminsearch(objective,c);
+%   Corrections to ensure over-prediction
+c(1) = 0.9998*c(1);
+c(2) = 1.000*c(2);
+%   Contract
+delL3 = delL(mask);
+iL3  = correlation(c,delL3);
+
+plot(delL(1:206),iL(1:206),[delL3;delL2],[iL3;iL2]);
+
+
+
+
+
+
+% =================================================== %
+%               Ice Coexistence Regions               %
+% =================================================== %
+
+% %   Melt curve: liquid side
+% [Pnd,tauM] = MeltingLineRND([5E3,0,0,0,0]);
+% delL0     = 1E3/CriticalDensity();
+% upupdater = @(delL,mask,r) [r./PressureOneRND_delta(delL,tauM(mask)),abs(r)];
+% updater   = @(delL,mask) upupdater(delL,mask,PressureOneRND(delL,tauM(mask)) - Pnd(mask));
+% delM      = NewtonUpdater(updater,Pnd*0+delL0,1E-15,30);
+% iNDM      = InternalEnergyOneRND(delM,tauM);
+% plot(delL2,iL2,delM,iNDM);
+
+
+% 
+% % =================================================== %
+% %                         Plot                        % 
+% % =================================================== %
+% 
+% figure(1);
+% 
+% subplot(2,2,1);
+%     semilogx(delG,iG,[delG1;delG2(2:end)],[iG1;iG2(2:end)]);
+%     xlabel('\delta_g')
+%     ylabel('i_{nd}');
+%     legend('Exact','Legend','Location','southwest');
+% subplot(2,2,2);
+%     loglog(delG,[iG1;iG2(2:end)] - iG);
+%     xlabel('\delta_g')
+%     ylabel('\Delta{i_{nd}}');
+% 
+% 
+% subplot(2,2,3);
+%     plot(delL,iL,[delL2;delL1(2:end)],[iL2;iL1(2:end)]);
+%     xlabel('\delta_l')
+%     ylabel('i_{nd}');
+%     legend('Exact','Legend','Location','southwest');
+% subplot(2,2,4);
+%     semilogy([delL2;delL1(2:end)],[iL2;iL1(2:end)] - iL(117:end));
+%     xlabel('\delta_l')
+%     ylabel('\Delta{i_{nd}}');
+% 
+
+
+
+
